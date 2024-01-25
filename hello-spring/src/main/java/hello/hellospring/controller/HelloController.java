bd691d01bdc3fb81fd2b908c683da9fdc3f5c351
redhean <redhean2000@naver.com>
2024-01-25 13:58:30 +0900

    Spring Basic Practice

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,54 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+
+        // name parameter 넘겨줄 때, ?name=spring 에서 띄어쓰기 하면 안 됨
+        // command p로 parameter 매개변수 확인 가능
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;   // 객체 반환 시,JSON type으로 반환
+    }
+
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
+
+// 스프링 부트 템플릿 엔진 기본 viewModel 매핑
+// resources:templates/ + '{ViewName}' + '.html'
+// return "hello"는 templates/hello.html을 return
